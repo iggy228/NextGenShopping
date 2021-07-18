43e34c50339ece346a6b566d1eb22d608c7e2485
cyber-T-800 <tadeas.kapina4@gmail.com>
2021-07-18 13:34:39 +0200

    add logic for login

--- app/src/main/java/sk/iggy/nextgenshopping/database/user/UserDao.kt
+++ app/src/main/java/sk/iggy/nextgenshopping/database/user/UserDao.kt
@@ -3,6 +3,7 @@ package sk.iggy.nextgenshopping.database.user
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import java.net.UnknownServiceException
 
 @Dao
 interface UserDao {
@@ -12,6 +13,8 @@ interface UserDao {
     @Query("SELECT * FROM users WHERE id = :id" )
     fun getById(id : Int) : User
 
+    @Query("SELECT * FROM users WHERE login_name = :loginName & password = :password")
+    fun getUser(loginName: String, password : String) : User
 
     @Insert
     fun insert(user: User)
